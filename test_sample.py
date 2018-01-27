cbf7c65098cf013f67cd32b8395be022c7c2f135
Siddharth Ramakrishnan <sid.tiger183@gmail.com>
2018-01-27 12:20:05 -0500

    Trying travis for reading input

--- test_sample.py
+++ test_sample.py
@@ -7,3 +7,9 @@ def test_answer():
 	arr1 = np.array([2])
 	arr2 = np.array([8])
 	assert func(arr1,arr2) == 0.25
+
+def test_input():
+	f = open("input.txt", "r")
+	contents =f.read()
+	assert f.mode == 'r'
+
